d2873ceb09488f3da82921251ba86f826eb69369
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-14 09:13:15 -0400

    Add event listener to button

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
+const input = document.getElementById('button')
 function addingEventListener() {
-}
+  const input = document.getElementById('button').addEventListener('click',() => alert('This was clikced'))}
+
